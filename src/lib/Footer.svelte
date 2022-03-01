aaf789bb6f7cbfba16d55e0dc287b0b9157cdf86
uf0 <scnepa222@gmail.com>
2022-03-01 16:53:49 +0100

    footer

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,47 @@
+<script>
+	import unipv from '$lib/assets/unipv.png';
+	import unibi from '$lib/assets/unibi.png';
+	import unica from '$lib/assets/unica.png';
+	import co from '$lib/assets/co.png';
+	import cariplo from '$lib/assets/cariplo.png';
+
+	const logos = {
+		unipv,
+		unibi,
+		unica,
+		co
+	};
+
+	const affiliations = [
+		{ label: 'Università di Pavia', link: '', logo: 'unipv' },
+		{ label: 'Università degli Studi di Milano Bicocca', link: '', logo: 'unibi' },
+		{ label: 'Università Cattolica di Milano', link: '', logo: 'unica' },
+		{ label: 'Careof', link: '', logo: 'co' }
+	];
+</script>
+
+<div class="container border-top border-secondary">
+	<div class="row">
+		<div class="col-12 my-4 Manrope-Variable fw-bold">
+			<div class="w-100 d-flex align-items-baseline mb-4">
+				<div>
+					<p class="text-primary mb-4">A research project by:</p>
+					{#each affiliations as affiliation}
+						<img class="me-5" src={logos[affiliation.logo]} height="65" alt={affiliation.label} />
+					{/each}
+				</div>
+				<div class="ms-auto">
+					<p class="text-primary mb-4 ">Funded by:</p>
+					<img src={cariplo} height="65" alt="Fondazione Cariplo" />
+				</div>
+			</div>
+			<p class="text-primary mb-0">Contact us:</p>
+			<p>
+				Drop a message to <a
+					class="text-decoration-none"
+					href="mailto:alessandro.caliandro@unipv.it">alessandro.caliandro@unipv.it</a
+				>
+			</p>
+		</div>
+	</div>
+</div>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,9 @@
 <script>
 	import '../app.scss';
 	import Navbar from '$lib/Navbar.svelte';
+	import Footer from '$lib/Footer.svelte';
 </script>
 
 <Navbar />
 <slot />
+<Footer />
