ecfced1414323b262b1da80fb3f970380b0256f0
uf0 <scnepa222@gmail.com>
2022-03-01 14:56:28 +0100

    wip home page

---
+++ src/lib/CardHome.svelte
@@ -0,0 +1,8 @@
+<script>
+	export let path, label;
+</script>
+
+<div>
+	<h4>{label}</h4>
+	<a href={path}>link</a>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,8 +4,26 @@
 
 <script>
 	import Index, { metadata } from './_index.md';
+	import CardHome from '$lib/CardHome.svelte';
+	const cards = [
+		{ path: '/team', label: 'Team' },
+		{ path: '/work-packages', label: 'Work Packages' },
+		{ path: '/outputs', label: 'Outputs' }
+	];
 </script>
 
-<h1>v-data</h1>
-<Index />
-<p>{metadata.subtitle}</p>
+<div class="container">
+	<div class="row">
+		<div class="col-12">
+			<p>{metadata.subtitle}</p>
+			<Index />
+		</div>
+	</div>
+	<div class="row">
+		{#each cards as card}
+			<div class="col-4">
+				<CardHome {...card} />
+			</div>
+		{/each}
+	</div>
+</div>
