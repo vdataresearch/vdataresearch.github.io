c73efcaeaa0a447596fc543cdd359fbd3ef58498
uf0 <scnepa222@gmail.com>
2022-03-01 18:22:16 +0100

    test

--- svelte.config.js
+++ svelte.config.js
@@ -8,10 +8,7 @@ const config = {
 	extensions: ['.svelte', ...mdsvexConfig.extensions],
 	preprocess: [mdsvex(mdsvexConfig), preprocess()],
 	kit: {
-		adapter: adapter({
-			pages: 'docs',
-			assets: 'docs'
-		}),
+		adapter: adapter(),
 		prerender: {
 			entries: ['*']
 		},
