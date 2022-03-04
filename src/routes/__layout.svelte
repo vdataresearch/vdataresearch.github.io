cec7022bc3d062320149c4e6d5c0cda54f8f4295
uf0 <scnepa222@gmail.com>
2022-03-04 15:37:50 +0100

    page transition

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,18 @@
+<script context="module">
+	export const load = async ({ url }) => ({ props: { url } });
+</script>
+
 <script>
 	import '../app.scss';
+	import PageTransition from '$lib/PageTransition.svelte';
 	import Navbar from '$lib/Navbar.svelte';
 	import Footer from '$lib/Footer.svelte';
+
+	export let url;
 </script>
 
 <Navbar />
-<slot />
+<PageTransition {url}>
+	<slot />
+</PageTransition>
 <Footer />
